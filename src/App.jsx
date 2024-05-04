c7600ea2818b5e4c453c85c33f8b4a0c242a82c5
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-04 08:07:38 +0100

    feat: desktop nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 
+import "./base-styles/main.scss";
+
 const Layout = () => {
   return (
     <div className="app">
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,32 @@
+import { NavLink, Link } from "react-router-dom";
+import Logo from "../../assets/icons/logo.png";
+
 const NavBar = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav className="nav">
+      <div className="nav__container">
+        <div className="nav__logo-wrapper">
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <ul className="nav__desktop-nav">
+          <NavLink className="nav__link" to="./">
+            Home
+          </NavLink>
+          <NavLink className="nav__link" to="./about">
+            About
+          </NavLink>
+          <NavLink className="nav__link" to="./prices">
+            Prices
+          </NavLink>
+          <NavLink className="nav__link" to="./contact">
+            Contact
+          </NavLink>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
