80c73bd91b81bbd24853f4aaf7be864c6096af46
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-04 07:20:02 +0100

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <>
-      <div></div>
+      <RouterProvider router={router} />
     </>
   );
 }
