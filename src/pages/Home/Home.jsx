7fd07d8f093c6631245cc076d6e20fd25ad18bf0
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-05 09:30:09 +0100

    feat: create + style hero section for home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import heroImg from "../../assets/images/woman-684686_1280.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home__hero">
+        <div className="home__hero-wrapper">
+          <div className="home__hero-text-wrapper">
+            <h1 className="home__hero-heading">You are worth it</h1>
+            <h2 className="home__hero-sub-heading">Best salon in Manchester</h2>
+            <Link className="home__hero-link">Book Now</Link>
+          </div>
+          <div className="home__hero-img-wrapper">
+            <img className="home__hero-img" src={heroImg} alt="smiling women" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
