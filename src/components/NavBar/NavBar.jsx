256e0a474445e33b2e635b729128d8b5642e23f6
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-05 07:06:26 +0100

    feat: mobile app

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import Logo from "../../assets/icons/logo.png";
+import menuCloseBtn from "../../assets/icons/close.png";
+import menuBtn from "../../assets/icons/burger-icon.png";
 
 const NavBar = () => {
+  const [isNavActive, setIsNavActive] = useState(false);
+
+  const closeNav = () => {
+    setIsNavActive(false);
+  };
+
+  const showNav = () => {
+    setIsNavActive(true);
+  };
+
   return (
     <nav className="nav">
       <div className="nav__container">
@@ -21,6 +34,28 @@ const NavBar = () => {
             Prices
           </NavLink>
           <NavLink className="nav__link" to="./contact">
+            Contact
+          </NavLink>
+        </ul>
+        <button className="nav__mobile-show-btn" onClick={showNav}>
+          <img src={menuBtn} alt="" />
+        </button>
+        <ul
+          className={isNavActive ? "nav__mobile-nav active" : "nav__mobile-nav"}
+        >
+          <button onClick={closeNav} className="nav__mobile-close-btn">
+            <img src={menuCloseBtn} alt="menu close button" />
+          </button>
+          <NavLink className="nav__link nav__link--mobile" to="./">
+            Home
+          </NavLink>
+          <NavLink className="nav__link nav__link--mobile" to="./about">
+            About
+          </NavLink>
+          <NavLink className="nav__link nav__link--mobile" to="./prices">
+            Prices
+          </NavLink>
+          <NavLink className="nav__link nav__link--mobile" to="./contact">
             Contact
           </NavLink>
         </ul>
