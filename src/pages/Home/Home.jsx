630431ae78404554b6eb3ab662122a5109c0bc0d
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-06 08:17:41 +0100

    feat: choose us section + styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import heroImg from "../../assets/images/woman-684686_1280.png";
 import razor from "../../assets/icons/razor.png";
 import aboutImg from "../../assets/images/aboutImg.jpg";
+import badge from "../../assets/icons/excellence-honor-icon.png";
+import handShake from "../../assets/icons/partner-handshake-icon.png";
+import comb from "../../assets/icons/hair-salon-icon.png";
 
 const Home = () => {
   return (
@@ -42,6 +45,50 @@ const Home = () => {
                 <img className="home-about__img" src={aboutImg} alt="" />
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-choose-us">
+        <h2 className="home-choose-us__heading">Why Choose Us</h2>
+        <div className="home-choose-us__wrapper">
+          <div className="home-choose-us__tile">
+            <div className="home-choose-us__icon-wrapper">
+              <img src={badge} alt="badge" />
+            </div>
+            <h3 className="home-choose-us__tile-heading"> Experience</h3>
+            <p className="home-choose-us__tile-text">
+              We have served our customers for over 10 years, with great
+              results. At La Bella Salon, our passion for beauty shines through
+              in every client interaction, ensuring a memorable experience that
+              leaves you feeling confident and radiant.
+            </p>
+          </div>
+          <div className="home-choose-us__tile">
+            <div className="home-choose-us__icon-wrapper">
+              <img src={handShake} alt="badge" />
+            </div>
+            <h3 className="home-choose-us__tile-heading"> Trust</h3>
+            <p className="home-choose-us__tile-text">
+              We are proud to have earned the trust of hundreds of satisfied
+              customers who continue to choose La Bella Salon for their beauty
+              needs time and time again. Our dedication to excellence and
+              commitment to customer satisfaction are reflected in the loyalty
+              of our clients, who trust us to consistently deliver exceptional
+              service and stunning results.
+            </p>
+          </div>
+          <div className="home-choose-us__tile">
+            <div className="home-choose-us__icon-wrapper">
+              <img src={comb} alt="badge" />
+            </div>
+            <h3 className="home-choose-us__tile-heading"> Knowledge</h3>
+            <p className="home-choose-us__tile-text">
+              At La Bella Salon, our team is made up of experienced and skilled
+              professionals. We`&apos`re dedicated to learning and staying
+              updated on the latest trends in the beauty industry. This means
+              you can trust us to provide you with the best service possible,
+              using the most current techniques and knowledge.
+            </p>
           </div>
         </div>
       </section>
