967f1b54e9b405bf5d1cf7fef9ba8fac4fa1e8a1
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-06 07:22:27 +0100

    feat: about secion + styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import heroImg from "../../assets/images/woman-684686_1280.png";
+import razor from "../../assets/icons/razor.png";
+import aboutImg from "../../assets/images/aboutImg.jpg";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="home__hero">
+      <section className="home__hero">
         <div className="home__hero-wrapper">
           <div className="home__hero-text-wrapper">
             <h1 className="home__hero-heading">You are worth it</h1>
@@ -15,7 +17,34 @@ const Home = () => {
             <img className="home__hero-img" src={heroImg} alt="smiling women" />
           </div>
         </div>
-      </div>
+      </section>
+      <section className="home-about">
+        <div className="home-about__wrapper">
+          <div className="home-about__tile">
+            <div className="home-about__icon-wrapper">
+              <img src={razor} alt="razor" />
+            </div>
+            <div className="home-about__text-wrapper">
+              <h2 className="home-about__heading">About Us</h2>
+              <p>
+                Welcome to La Bella Salon, where beauty and relaxation
+                intertwine seamlessly. Our expert team of stylists and beauty
+                professionals is dedicated to providing personalized services to
+                enhance your natural beauty and leave you feeling rejuvenated.
+                Step into our luxurious oasis and experience the perfect blend
+                of pampering and sophistication.
+              </p>
+            </div>
+          </div>
+          <div className="home-about__tile">
+            <div className="home-about__img-wrapper">
+              <div className="home-about__img-wrapper">
+                <img className="home-about__img" src={aboutImg} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
