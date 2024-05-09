81cd766d6e2e62ed8a6e6c41b1a40f71ae41b923
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-09 06:41:03 +0100

    feat: training section + styles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,12 @@
 import Banner from "../../components/Banner/Banner";
 import alice from "../../assets/images/About-page-image.png";
+import bride from "../../assets/images/bride.png";
 
 const About = () => {
   return (
     <div>
       <Banner heading={"About Us"} subheading={"Bit More About Us"} />
-      <secion className="about">
+      <section className="about">
         <div className="about__welcom-section-wrapper">
           <div className="about__welcom-img-wrapper">
             <img src={alice} alt="Owner" className="about__welcome-img" />
@@ -28,7 +29,31 @@ const About = () => {
             </p>
           </div>
         </div>
-      </secion>
+      </section>
+      <section className="about-training">
+        <h2 className="about-training__heading">Training</h2>
+        <div className="about-training__wrapper">
+          <div className="about-training__text-wrapper">
+            <h3 className="about-training__subheading">Our Commitment</h3>
+            <p>
+              At La Bella Salon, we understand the importance of looking your
+              best on those special occasions. That&apos;s why we&apos;re
+              dedicated to staying up-to-date with the latest trends and
+              continuously expanding our skills. By prioritizing ongoing
+              training and education, we ensure that we can always offer you the
+              latest techniques and styles to make you look and feel amazing for
+              any event.
+            </p>
+          </div>
+          <div className="about-training__img-wrapper">
+            <img
+              className="about-training__img"
+              src={bride}
+              alt="bride hair style"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
