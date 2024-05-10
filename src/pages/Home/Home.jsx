22f002913b8239ea8e8d47f6c37d7ba3fcdf07c3
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-10 07:48:18 +0100

    fix: fixed cta lilnk hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,9 @@ const Home = () => {
           <div className="home__hero-text-wrapper">
             <h1 className="home__hero-heading">You are worth it</h1>
             <h2 className="home__hero-sub-heading">Best salon in Manchester</h2>
-            <Link className="home__hero-link">Book Now</Link>
+            <Link to="./contact" className="home__hero-link">
+              Book Now
+            </Link>
           </div>
           <div className="home__hero-img-wrapper">
             <img className="home__hero-img" src={heroImg} alt="smiling women" />
