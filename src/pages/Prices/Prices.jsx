f47ff6aa6bd8d0bfc7f1e4a78412d45b3b74532b
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-05-10 06:39:50 +0100

    fix: remove border bottom from last element

--- src/pages/Prices/Prices.jsx
+++ src/pages/Prices/Prices.jsx
@@ -17,34 +17,40 @@ const Prices = () => {
       </section>
       <section className="prices">
         <table className="prices__table">
-          <tr className="prices__table-head">
-            <th className="prices__row">Our Standard Prices</th>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">Women&apos;s Haircut: £40</td>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">Men&apos;s Haircut: £20</td>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">Blowout and Styling: £35</td>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">Highlights (Full Head): £100</td>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">Balayage: £120</td>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">
-              Hair Color (Root Touch-Up): £60
-            </td>
-          </tr>
-          <tr>
-            <td className="prices__table-cell">
-              Hair Extensions (Consultation Required): Starting from £200
-            </td>
-          </tr>
+          <thead>
+            <tr className="prices__table-head">
+              <th className="prices__row">Our Standard Prices</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="prices__table-cell">Women&apos;s Haircut: £40</td>
+            </tr>
+            <tr>
+              <td className="prices__table-cell">Men&apos;s Haircut: £20</td>
+            </tr>
+            <tr>
+              <td className="prices__table-cell">Blowout and Styling: £35</td>
+            </tr>
+            <tr>
+              <td className="prices__table-cell">
+                Highlights (Full Head): £100
+              </td>
+            </tr>
+            <tr>
+              <td className="prices__table-cell">Balayage: £120</td>
+            </tr>
+            <tr>
+              <td className="prices__table-cell">
+                Hair Color (Root Touch-Up): £60
+              </td>
+            </tr>
+            <tr>
+              <td className="prices__table-cell prices__table-cell--last">
+                Hair Extensions (Consultation Required): Starting from £200
+              </td>
+            </tr>
+          </tbody>
         </table>
       </section>
     </div>
